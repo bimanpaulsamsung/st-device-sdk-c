9bc1c937c1250289164f482ee213b4aaaa5f9804
Kwang-Hui Cho <54692798+Kwang-Hui@users.noreply.github.com>
2020-04-13 17:51:57 +0900

    [Stage] test: add CTS application for posix
    CTS application doesn't uses mocking.
    So we can't use it with UnitTest build environment.
    CTS for posix will be compiled with Debug build option.

---
+++ test/CTS_posix_main.c
@@ -0,0 +1,33 @@
+/* ***************************************************************************
+ *
+ * Copyright (c) 2020 Samsung Electronics All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include <stdio.h>
+#include "framework/cts/include/cts.h"
+
+int main(void)
+{
+    int err = 0;
+
+    err += CTS_iot_bsp_test();
+    err += CTS_iot_os_test();
+
+    return err;
+}
