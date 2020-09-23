d7768352656d4d90437707a7c553c89e75bdc34b
Biman Paul <biman.paul@samsung.com>
2020-09-23 10:11:48 +0530

    easysetup: implement stm32 AT command based apis

--- src/easysetup/http/tcp/iot_easysetup_http_tcp.c
+++ src/easysetup/http/tcp/iot_easysetup_http_tcp.c
@@ -15,9 +15,12 @@
  * language governing permissions and limitations under the License.
  *
  ****************************************************************************/
+#define MBEDOS_STM32
 
 #include <string.h>
+#if !defined(MBEDOS_STM32)
 #include <sys/socket.h>
+#endif
 #include <errno.h>
 #if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
 #include <netinet/in.h>
@@ -28,6 +31,13 @@
 #include "iot_os_util.h"
 #include "iot_debug.h"
 #include "iot_easysetup.h"
+
+
+#if defined(MBEDOS_STM32)
+#include "wifi.h"
+#define WIFI_WRITE_TIMEOUT 10000
+#define WIFI_READ_TIMEOUT  1000
+#endif
 
 #define PORT 8888
 #define RX_BUFFER_MAX    1024
@@ -40,6 +50,7 @@ static int listen_sock = -1;
 static int accept_sock = -1;
 static int deinit_processing = 0;
 
+#if !defined(MBEDOS_STM32)
 static void _clear_sockets(void)
 {
 	if (listen_sock != -1) {
@@ -56,6 +67,22 @@ static void _clear_sockets(void)
 	}
 }
 
+#else
+iot_os_mutex atmutex;
+
+static void _clear_sockets(void)
+{
+	iot_os_mutex_lock(&atmutex);
+	if (accept_sock != -1) {
+		IOT_INFO("close accept socket");
+		WIFI_StopServer(accept_sock);
+		accept_sock = -1;
+	}
+	iot_os_mutex_unlock(&atmutex);
+}
+#endif
+
+#if !defined(MBEDOS_STM32)
 static int _process_accept_socket(int sock)
 {
 	char rx_buffer[RX_BUFFER_MAX];
@@ -182,6 +209,151 @@ static int _process_accept_socket(int sock)
 	return 0;
 }
 
+#else
+static int _process_accept_socket(int sock)
+{
+	char rx_buffer[RX_BUFFER_MAX];
+	int rx_buffer_len = 0;
+	int http_request_header_len = 0;
+	iot_error_t err = IOT_ERROR_NONE;
+	size_t content_len = 0;
+
+	char *payload;
+	int i, ret, type, cmd;
+	uint16_t len;
+	int wifi_code;
+
+
+	while (1)
+	{
+		// start to process one http request
+		http_request_header_len = -1;
+		memset(rx_buffer, '\0', sizeof(rx_buffer));
+		rx_buffer_len = 0;
+		content_len = 0;
+
+		// ensure complete http request header before es_msg_parser
+		do {
+			iot_os_mutex_lock(&atmutex);
+			wifi_code = WIFI_ReceiveData(sock, rx_buffer + rx_buffer_len, sizeof(rx_buffer) - rx_buffer_len - 1, &len, WIFI_READ_TIMEOUT);
+			iot_os_mutex_unlock(&atmutex);
+
+			if (wifi_code != WIFI_STATUS_OK) {
+				IOT_ERROR("recv failed: errno %d", wifi_code);
+				return -1;
+			} else {
+				char *start = NULL;
+				//remove AT command OK delimiter
+				start = rx_buffer + rx_buffer_len + len;
+				for (int i=0; i< 8; i++) {
+					start[i] = 0;
+				}
+			}
+
+			if (len < 0) {
+				if (!deinit_processing) {
+					IOT_ERROR("recv failed: errno %d", errno);
+					IOT_ES_DUMP(IOT_DEBUG_LEVEL_ERROR, IOT_DUMP_EASYSETUP_SOCKET_RECV_FAIL, errno);
+				}
+				return -1;
+			}
+			else if (len == 0) {
+				IOT_ERROR("Connection closed");
+				IOT_ES_DUMP(IOT_DEBUG_LEVEL_ERROR, IOT_DUMP_EASYSETUP_SOCKET_CON_CLOSE, 0);
+				return -1;
+			}
+			else {
+				rx_buffer_len += len;
+			}
+
+			// \r\n\r\n  header end
+			for (i = 0; i < rx_buffer_len; i++) {
+				if (i < rx_buffer_len - 3) {
+					if ((rx_buffer[i] == '\r') && (rx_buffer[i + 1] == '\n') && (rx_buffer[i + 2] == '\r')
+						&& (rx_buffer[i + 3] == '\n')) {
+						http_request_header_len = i + 4;
+					}
+				}
+			}
+		} while (http_request_header_len < 0);
+
+		err = es_msg_parser(rx_buffer, sizeof(rx_buffer), &payload, &cmd, &type, &content_len);
+
+		if ((err == IOT_ERROR_NONE) && (type == D2D_POST)
+				&& payload && (content_len > strlen((char *)payload)))
+		{
+			do {
+				iot_os_mutex_lock(&atmutex);
+				wifi_code = WIFI_ReceiveData(sock, rx_buffer + rx_buffer_len, sizeof(rx_buffer) - rx_buffer_len - 1, &len, WIFI_READ_TIMEOUT);
+				iot_os_mutex_unlock(&atmutex);
+
+				if (wifi_code != WIFI_STATUS_OK) {
+					IOT_ERROR("recv failed: errno %d", wifi_code);
+					return -1;
+				} else {
+					char *start = NULL;
+					start = rx_buffer + rx_buffer_len + len;
+					for (int i=0; i< 8; i++) {
+						start[i] = 0;
+					}
+				}
+
+				if (len < 0) {
+					IOT_ERROR("recv failed: errno %d", errno);
+					IOT_ES_DUMP(IOT_DEBUG_LEVEL_ERROR, IOT_DUMP_EASYSETUP_SOCKET_RECV_FAIL, errno);
+					return -1;
+				}
+				else if (len == 0) {
+					IOT_ERROR("Connection closed");
+					IOT_ES_DUMP(IOT_DEBUG_LEVEL_ERROR, IOT_DUMP_EASYSETUP_SOCKET_CON_CLOSE, 0);
+					return -1;
+				}
+				else {
+					rx_buffer_len += len;
+				}
+			} while (rx_buffer_len < (http_request_header_len + content_len));
+
+			payload = rx_buffer + http_request_header_len;
+		}
+
+
+		if(err == IOT_ERROR_INVALID_ARGS)
+			http_msg_handler(cmd, &tx_buffer, D2D_ERROR, payload);
+		else
+			http_msg_handler(cmd, &tx_buffer, type, payload);
+
+		if (!tx_buffer) {
+			IOT_ERROR("tx_buffer is NULL");
+			IOT_ES_DUMP(IOT_DEBUG_LEVEL_ERROR, IOT_DUMP_EASYSETUP_INTERNAL_SERVER_ERROR, 0);
+			return -1;
+		}
+
+		len = strlen((char *)tx_buffer);
+		tx_buffer[len] = 0;
+
+		iot_os_mutex_lock(&atmutex);
+		wifi_code = WIFI_SendData(sock, (uint8_t *)tx_buffer, len, &ret, WIFI_WRITE_TIMEOUT);
+		iot_os_mutex_unlock(&atmutex);
+
+		if (wifi_code != WIFI_STATUS_OK) {
+			IOT_ERROR("send failed: errno %d", wifi_code);
+			return -1;
+		}
+
+		free(tx_buffer);
+		tx_buffer = NULL;
+		if (ret < 0) {
+			IOT_ERROR("Error is occurred during sending: errno %d", ret);
+			IOT_ES_DUMP(IOT_DEBUG_LEVEL_ERROR, IOT_DUMP_EASYSETUP_SOCKET_SEND_FAIL, ret);
+			return -1;
+		}
+	}
+
+	return 0;
+}
+#endif
+
+#if !defined(MBEDOS_STM32)
 static void es_tcp_task(void *pvParameters)
 {
 	int addr_family, ip_protocol, ret;
@@ -258,6 +430,53 @@ static void es_tcp_task(void *pvParameters)
 	es_tcp_task_handle = NULL;
 	iot_os_thread_delete(NULL);
 }
+#else
+int32_t Socket = -1;
+static void es_tcp_task(void *pvParameters)
+{
+	int ret;
+
+	Socket = 0;
+
+
+	while (!deinit_processing) {
+		IOT_INFO("Starting Server");
+		ret = WIFI_StartServer(Socket, WIFI_TCP_PROTOCOL, "EasysetupHttpd", PORT);
+		IOT_INFO("WIFI_StartServer Status: %d", ret);
+		if(ret == WIFI_STATUS_OK)
+		{
+			IOT_INFO("Accepted Connection");
+			accept_sock = Socket;
+		}
+		else
+		{
+			accept_sock = -1;
+			IOT_ERROR("ERROR : Connection cannot be established.");
+		}
+
+		while (1) {
+
+			if (accept_sock >= 0 && !deinit_processing)
+				if (_process_accept_socket(accept_sock) < 0)
+					break;
+			else
+				break;
+
+		}
+
+		IOT_INFO("close accept socket");
+		_clear_sockets();
+	}
+
+	if (!deinit_processing) {
+		_clear_sockets();
+	}
+
+	/*set es_tcp_task_handle to null, prevent dulicate delete in es_tcp_deinit*/
+	es_tcp_task_handle = NULL;
+	iot_os_thread_delete(NULL);
+}
+#endif
 
 void es_http_init(void)
 {
@@ -265,6 +484,9 @@ void es_http_init(void)
 	IOT_ES_DUMP(IOT_DEBUG_LEVEL_INFO, IOT_DUMP_EASYSETUP_TCP_INIT, 0);
 	iot_os_thread_create(es_tcp_task, "es_tcp_task", (1024 * 4), NULL, 5, (iot_os_thread * const)(&es_tcp_task_handle));
 	IOT_ES_DUMP(IOT_DEBUG_LEVEL_INFO, IOT_DUMP_EASYSETUP_TCP_INIT, 1);
+
+	IOT_INFO("RETURN>>>");
+	iot_os_mutex_init(&atmutex);
 }
 
 void es_http_deinit(void)
@@ -286,6 +508,7 @@ void es_http_deinit(void)
 	}
 
 	deinit_processing = 0;
+	iot_os_mutex_destroy(&atmutex);
 	IOT_INFO("http tcp deinit complete!");
 	IOT_ES_DUMP(IOT_DEBUG_LEVEL_INFO, IOT_DUMP_EASYSETUP_TCP_DEINIT, 1);
 }
