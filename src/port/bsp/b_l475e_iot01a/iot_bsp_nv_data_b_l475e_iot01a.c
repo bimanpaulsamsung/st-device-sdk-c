2e12ca7ed2329cf0b91f0a053f95c69c22437967
Biman Paul <biman.paul@samsung.com>
2021-05-06 20:58:03 +0530

    stm32l4: filesystem NV data

--- src/port/bsp/b_l475e_iot01a/iot_bsp_nv_data_b_l475e_iot01a.c
+++ src/port/bsp/b_l475e_iot01a/iot_bsp_nv_data_b_l475e_iot01a.c
@@ -18,8 +18,57 @@
 
 
 #include "iot_bsp_nv_data.h"
+#include "iot_debug.h"
 
-const char* iot_bsp_nv_get_data_path(iot_nvd_t nv_type)
-{
-	return NULL;
+const char* iot_bsp_nv_get_data_path(iot_nvd_t nv_type) {
+	HIT();
+	IOT_WARN_CHECK((nv_type < 0 || nv_type > IOT_NVD_MAX), NULL,
+			"Invalid args");
+
+	switch (nv_type) {
+
+	/* wifi prov data */
+	case IOT_NVD_WIFI_PROV_STATUS:
+		return "/fs/WifiProvStatus";
+	case IOT_NVD_AP_SSID:
+		return "/fs/IotAPSSID";
+	case IOT_NVD_AP_PASS:
+		return "/fs/IotAPPASS";
+	case IOT_NVD_AP_BSSID:
+		return "/fs/IotAPBSSID";
+	case IOT_NVD_AP_AUTH_TYPE:
+		return "/fs/IotAPAuthType";
+
+	/* cloud prov data */
+	case IOT_NVD_CLOUD_PROV_STATUS:
+		return "/fs/CloudProvStatus";
+	case IOT_NVD_SERVER_URL:
+		return "/fs/ServerURL";
+	case IOT_NVD_SERVER_PORT:
+		return "/fs/ServerPort";
+	case IOT_NVD_LABEL:
+		return "/fs/Label";
+	case IOT_NVD_DEVICE_ID:
+		return "/fs/DeviceID";
+	case IOT_NVD_MISC_INFO:
+		return "/fs/MiscInfo";
+
+	/* TODO: Get Manufacturer data */
+	/* stored in stnv partition (manufacturer data) */
+	case IOT_NVD_PRIVATE_KEY:
+		return "/rom/PrivateKey";
+	case IOT_NVD_PUBLIC_KEY:
+		return "/rom/PublicKey";
+	case IOT_NVD_ROOT_CA_CERT:
+		return "/rom/RootCert";
+	case IOT_NVD_SUB_CA_CERT:
+		return "/rom/SubCert";
+	case IOT_NVD_DEVICE_CERT:
+		return "/rom/DeviceCert";
+	case IOT_NVD_SERIAL_NUM:
+		return "/rom/SerialNum";
+
+	default:
+		return NULL;
+	}
 }
